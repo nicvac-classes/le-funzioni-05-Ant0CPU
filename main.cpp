41a93361cb52841d1a83a61b4d9fb0f4ea59260f
Ant0CPU <antonio.bruno@marconibari.edu.it>
2025-12-10 21:51:32 +0000

    Consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,62 @@
 #include <iostream>
+#include <vector>
+#include <ctime>
+#include <cmath>
 using namespace std;
 
+void ordina(int n, vector<int> & v) {
+    
+    int i(0), j(0), t;
+    bool scambi(true);
+
+    while (i<=n-1 && scambi) {
+        scambi=false;
+        j=0;
+
+        while (j<=(n-2)-i) {
+            if (v[j]>v[j+1]) {
+
+                t=v[j];
+                v[j]=v[j+1];
+                v[j+1]=t;
+
+                scambi=true;
+            }
+            j=j+1;
+        }
+        i=i+1;
+    }
+}
+
+void stampa(int n, vector<int> & v) {
+    int i;
+
+    cout<<"Vettore: ";
+    for (i=0; i<=n-1; i++) {
+        cout<<v[i]<<"; ";
+    }
+    cout<<""<<endl;
+}
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+
+    srand(time(0));
+
+    int c(5), i(0), j;
+    vector<int> v(c), w(c);
+
+    while (i<=c-1) {
+        v[i]=rand() % c*10+1;
+        w[i]=rand() % c*10;
+
+        i=i++;
+    }
+
+    ordina(c, v);
+    ordina(c, w);
+    stampa(c, v);
+    stampa(c, w);
+
+    return 0;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
